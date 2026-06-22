6da69a6eb7be46edb8a248b2dd7efb1f0eb9fbf4
OBASEKORE, Hammed Olatunde <obasekore.hammed@gmail.com>
2026-06-23 05:13:51 +0900

    feat: add simulation of step response of the motor dynamics

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from dc_motor_env import motor_dynamics
+
+
+def simulate_step_response():
+    # Simulate step response
+    sol = solve_ivp(motor_dynamics, [0, 2], [0, 0], args=(12,), 
+                    t_eval=np.linspace(0, 2, 500))
+
+    plt.figure(figsize=(12, 6))
+    plt.subplot(2, 1, 1)
+    plt.plot(sol.t, sol.y[0])
+    plt.ylabel('Angular velocity (rad/s)')
+    plt.xlabel('Time (s)')
+    plt.title('Step Response of DC Motor')
+    plt.grid(True)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(sol.t, sol.y[1])
+    plt.ylabel('Current (A)')
+    plt.xlabel('Time (s)')
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    simulate_step_response()
